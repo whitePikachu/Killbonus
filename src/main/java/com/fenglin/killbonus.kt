096ca24ea449d707b624d84f4b2c1ec2a6547863
枫叶秋林 <liuqianpan2018@outlook.com>
2022-12-06 16:34:51 +0800

    项目初始化

---
+++ src/main/java/com/fenglin/killbonus.kt
@@ -0,0 +1,13 @@
+package com.fenglin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class killbonus : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
